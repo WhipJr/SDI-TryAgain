63bd19e087e8fb0870c30de264274b74dd362700
Christian <christianswinford@yahoo.com>
2014-11-05 15:53:57 -0500

    commented work
    added comments throughout the script to help make things easier to read

--- Swinford_Christian_Assignment1.js
+++ Swinford_Christian_Assignment1.js
@@ -14,26 +14,32 @@ var gameList = [
 var number; //used for game selection
 
 var playGame = confirm("Would you like to play a game?");
-if(playGame){askUser();}
-else {end();}
-//prompt: ask user a question.
+if(playGame){
+	askUser();
+}
+else{
+	end();
+}
+//prompt: ask user which game to play.
 function askUser(){
 	console.log(gameList);
 	number = parseInt(prompt("What Game do you want to play? Please enter 1-" +(gameList.length) +" only.", "1"));
-
+	//check to see if the user selected a number greater than the allowed number of games
 	if(number>=gameList.length+1)
 	{
 		console.log("the number you entered is incorrect, please try again.")
 		askUser();
 	}
+	//check to see if the user selected anything less than 1
 	else if(number<=0){
 		console.log("the number you entered is incorrect, please try again.")
 		askUser();
 	}
+	//if neither of those occurred, continue to prompt confirmation of the correct game.
 	else
 	{
-
 		playGame = confirm("Are you sure you would like to play: \n" + gameList[number-1] + "?")
+		//if user does not want to play the selected game, restart the process.
 		if(!playGame){ 
 			playGame = confirm("Would you like to play a game?");
 			if(playGame){
@@ -87,8 +93,10 @@ function startGame(gameNum)
 
 //call to temporarily pause script for x amount of seconds.
 function load(seconds) {
+	//get current time
 	var start = new Date().getTime();
 	for (var i = 0; i < 1e7; i++) {
+		//take current time and find the difference from now and when function started
 		if ((new Date().getTime() - start) > (seconds+1000)){
 			break;
 		}
