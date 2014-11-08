7751924d32a3c954574203d27febabed7e2d7b48
Christian <christianswinford@yahoo.com>
2014-11-08 15:08:18 -0500

    created JSON data structure
    I Have created the JSON Data .js file and added the script tag in the
    HTML document.

---
+++ json.js
@@ -0,0 +1,28 @@
+/*
+ * Christian Swinford's JSON Data
+ */
+
+var json = {
+		"Games": [
+		{
+			"Name": "Call of Duty: Advanced Warfare",
+			"Size": "45.9 GB",
+			"Playable": true
+		},
+		{
+			"Name": "Battlefield 4",
+			"Size": "39.6 GB",
+			"Playable": true
+		},
+		{
+			"Name": "Thief",
+			"Size": "21.2 GB",
+			"Playable": true
+		},
+		{
+			"Name": "Evolve",
+			"Size": "12.1 GB",
+			"Playable": false
+		}
+	]	
+};
